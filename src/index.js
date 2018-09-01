81a7c73fc19ee25d94c05281682e2c2abe193f88
sophia1215 <code.sophiawang@gmail.com>
2018-09-01 19:46:04 +0800

    react-router example: Custom Link

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 // import UrlParams from './UrlParams';
-import AuthWorkFlow from './AuthWorkFlow';
+// import AuthWorkFlow from './AuthWorkFlow';
+import CustomLink from './CustomLink';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<UrlParams />, document.getElementById('root'));
-ReactDOM.render(<AuthWorkFlow />, document.getElementById('root'));
+// ReactDOM.render(<AuthWorkFlow />, document.getElementById('root'));
+ReactDOM.render(<CustomLink />, document.getElementById('root'));
+
+
+
+
