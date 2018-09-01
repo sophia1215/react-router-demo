44b2ed9f8d20bd5a3199c3610aff6975b0a10d01
sophia1215 <code.sophiawang@gmail.com>
2018-09-01 20:28:37 +0800

    react-router example: Preventing Transitions

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 // import UrlParams from './UrlParams';
 // import AuthWorkFlow from './AuthWorkFlow';
-import CustomLink from './CustomLink';
+// import CustomLink from './CustomLink';
+import PreventingTransitions from './PreventingTransitions';
+
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<UrlParams />, document.getElementById('root'));
 // ReactDOM.render(<AuthWorkFlow />, document.getElementById('root'));
-ReactDOM.render(<CustomLink />, document.getElementById('root'));
-
+// ReactDOM.render(<CustomLink />, document.getElementById('root'));
+ReactDOM.render(<PreventingTransitions />, document.getElementById('root'));
 
 
 
