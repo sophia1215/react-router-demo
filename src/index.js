a566e3036571cb14fe9c670e2a62c2fb088864eb
sophia1215 <code.sophiawang@gmail.com>
2018-09-01 16:41:40 +0800

    react-router example: URL Paramenters

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import UrlParams from './UrlParams';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<UrlParams />, document.getElementById('root'));
