103c168431853c276b1d1b3c3fc63c9237e91307
sophia1215 <code.sophiawang@gmail.com>
2018-09-01 17:28:58 +0800

    react-router example: Auth WorkFlow (Redirects)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
-import UrlParams from './UrlParams';
+// import UrlParams from './UrlParams';
+import AuthWorkFlow from './AuthWorkFlow';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<UrlParams />, document.getElementById('root'));
+// ReactDOM.render(<UrlParams />, document.getElementById('root'));
+ReactDOM.render(<AuthWorkFlow />, document.getElementById('root'));
