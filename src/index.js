95968efce8812aca7ef6fcd4b05e2fdc91daad62
sophia1215 <code.sophiawang@gmail.com>
2018-09-02 13:23:10 +0800

    react-router example: No Matche

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 // import UrlParams from './UrlParams';
 // import AuthWorkFlow from './AuthWorkFlow';
 // import CustomLink from './CustomLink';
-import PreventingTransitions from './PreventingTransitions';
-
+// import PreventingTransitions from './PreventingTransitions';
+import NoMatch from './NoMatch';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<UrlParams />, document.getElementById('root'));
 // ReactDOM.render(<AuthWorkFlow />, document.getElementById('root'));
 // ReactDOM.render(<CustomLink />, document.getElementById('root'));
-ReactDOM.render(<PreventingTransitions />, document.getElementById('root'));
-
+// ReactDOM.render(<PreventingTransitions />, document.getElementById('root'));
+ReactDOM.render(<NoMatch />, document.getElementById('root'));
 
 
