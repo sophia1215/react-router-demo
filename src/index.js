7c65aab8fae60a1f7e90c6bda2a0f624d2b6c82a
sophia1215 <code.sophiawang@gmail.com>
2018-09-02 13:53:55 +0800

    react-router example: Recursive Paths

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import ReactDOM from 'react-dom';
 // import AuthWorkFlow from './AuthWorkFlow';
 // import CustomLink from './CustomLink';
 // import PreventingTransitions from './PreventingTransitions';
-import NoMatch from './NoMatch';
+// import NoMatch from './NoMatch';
+import RecursivePaths from './RecursivePaths';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<UrlParams />, document.getElementById('root'));
 // ReactDOM.render(<AuthWorkFlow />, document.getElementById('root'));
 // ReactDOM.render(<CustomLink />, document.getElementById('root'));
 // ReactDOM.render(<PreventingTransitions />, document.getElementById('root'));
-ReactDOM.render(<NoMatch />, document.getElementById('root'));
-
+// ReactDOM.render(<NoMatch />, document.getElementById('root'));
+ReactDOM.render(<RecursivePaths />, document.getElementById('root'));
 
